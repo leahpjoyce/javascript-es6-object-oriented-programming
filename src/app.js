ceeb559ece8e31f781f575debaf5ddd91218ebad
Leah Joyce <leahpjoyce@gmail.com>
2018-09-13 21:37:47 -0500

    feat: Contructors and Properties

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
-let droneId = 5;
+class Drone {
+  constructor(id, name) {
+    console.log(id + ' ' + name);
+  }
 
-console.log(droneId);
+}
+
+let drone = new Drone(1234, 'Leah');
+
+//console.log(typeof Drone);
+//console.log(typeof drone);
+//console.log(drone instanceof Drone);
